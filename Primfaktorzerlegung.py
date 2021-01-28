d41bde4dd36470e952c06f1b55cd4c8da63e962f
Stephan1403 <Stephanf2003@gmail.com>
2021-01-28 18:24:28 +0100

    Update Primfaktorzerlegung.py

--- Primfaktorzerlegung.py
+++ Primfaktorzerlegung.py
@@ -1,12 +1,12 @@
 
 #________Primfaktorzerlegung___________
 
-#überprüft, ob es sich um eine primzahl handelt, indem sie prüft, ob sie durch irgendeine Zahl teilbar ist außer 1 und sich selbst
+
 def isPrimzahl(zahl):
     faktor = zahl   
 
-    for i in range (1, zahl - 1 ):                       #schleife die die von 1 bis zu (zahl - 1) durchläuft
-        if zahl % (faktor - i) == 0:                     #überprüft beim ersten mal, ob zahl / zahl-1 ohne Rest teilbar ist. Die nächsten Durchgänge jeweils wird der Nenner um 1 veringert
+    for i in range (1, zahl - 1 ):                       
+        if zahl % (faktor - i) == 0:                    
             return False
         
     return True
@@ -15,7 +15,7 @@ def isPrimzahl(zahl):
 def nextPrimzahl(letzteZahl):
     aktuelleZahl = letzteZahl + 1
 
-    while not isPrimzahl(aktuelleZahl):                  #solange die aktuelleZahl keine Primzahl ist wird sie um 1 erhöht
+    while not isPrimzahl(aktuelleZahl):
         aktuelleZahl = aktuelleZahl + 1 
 
     return aktuelleZahl
@@ -36,20 +36,18 @@ if __name__ == "__main__":
     else:
 
         while True: 
-            if zahl % primfaktor == 0:                      #wenn ja, dann ist primfaktor der erste faktor
-                primfaktoren.append(primfaktor)             #primfaktor wird zur liste hinzugefügt
-                zahl = zahl / primfaktor                    #der primfaktor wird einmal aus der zahl gezogen
-                primfaktor = 2                              #primfaktor wird zurück gesetzt
-                if zahl == 1:                               #bricht nach letztem Versuch
+            if zahl % primfaktor == 0:                     
+                primfaktoren.append(primfaktor)             
+                zahl = zahl / primfaktor                    
+                primfaktor = 2                              
+                if zahl == 1:                               
                     break
 
             else:
-                primfaktor = nextPrimzahl(primfaktor)       #der nächst hohe primfaktor ersetzt den aktuellen primfaktoren
+                primfaktor = nextPrimzahl(primfaktor)       
 
 
         
-
-    #result
     print("Die Zahl " +str(int(zahlInput)) + " kann in folgende Primfaktoren zerlegt werden: \n" + str(primfaktoren))
 
 
